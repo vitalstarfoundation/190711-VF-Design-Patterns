fb4d4f96c3531cd33612ea91e3ee8979d1319af6
mc <mc>
2019-08-22 21:38:04 -0700

    refactoring

--- src/test/scala/org/vitalstar/TestComposite.scala
+++ src/test/scala/org/vitalstar/TestComposite.scala
@@ -7,59 +7,44 @@ import org.junit.runner.RunWith
 
 import collection.mutable.ListBuffer
 
-trait Valuable {
-  def cost: Double
-}
-
 // We define a concept, which is behavior
 trait FoodItemish {
-  def name: String
-  def sold: Boolean
+  def name: String = ""
+  def sold: Boolean = false
   def consumed: Boolean = sold
-  def calories: Double
-  def addFoodItem(item: FoodItemish)
-  def getFoodItems: List[FoodItemish]
+  def calories: Double = 0.0
+  def addFoodItem(item: FoodItemish) = {}
+  def getFoodItems: List[FoodItemish] = List()
   def price: Double
-  def isSingle: Boolean
+  def isSingle: Boolean = true
 }
 
 // This is the implementation
-abstract class FoodItemImpl(_name: String ="", val _price: Double = 0.0) extends FoodItemish{
-  val name: String = _name
+abstract class FoodItemImpl(_name: String ="", val _price: Double = 0.0) extends FoodItemish {
+  override val name: String = _name
   val items: ListBuffer[FoodItemish] = new ListBuffer[FoodItemish]()
-  def addFoodItem(item: FoodItemish): Unit = items.append(item)
-  def getFoodItems: List[FoodItemish] = items.toList
-  def price: Double = {
+  override def addFoodItem(item: FoodItemish): Unit = items.append(item)
+  override def getFoodItems: List[FoodItemish] = items.toList
+  override def price: Double = {
     if (isSingle)
         _price
     else
-      items.foldLeft(0.0){(price, i) => i.price + price}
+      items.foldLeft(0.0){(price: Double, i: FoodItemish) => i.price + price}
   }
   override def toString: String = name
 }
 
+class Toy extends FoodItemish {
+  def price = 100
+}
+
 class Food(name:String, _price: Double) extends FoodItemImpl(name, _price) {
-  def isSingle = true
-  def discounted80: Double = discountPrice(0.8)
-  def discountPrice(_discount: Double) = price * _discount
-  def cost = 1
-
-  def sold: Boolean = false
-  def calories: Double = 0
-
   override def addFoodItem(item: FoodItemish): Unit = {}
   override def getFoodItems: List[FoodItemish] = List(this)
-
 }
 
 class Meal(name: String)  extends FoodItemImpl(name) {
-  def isSingle = false
-//  def discountPrice(_discount: Double) = a.discountPrice(_discount) + b.discountPrice(_discount) + c.discountPrice(_discount)
-//  def discountPrice(_discount: Double) = price * _discount
-//  def cost = a.cost + b.cost + c.cost
-
-  def sold: Boolean = false
-  def calories: Double = 0
+  override def isSingle = false
   override def toString = name + " " + getFoodItems.toString
 }
 
@@ -76,8 +61,10 @@ class TestComposite extends FunSuite with BeforeAndAfterAll {
     breakfast.addFoodItem(burger)
     breakfast.addFoodItem(drink)
 
+    val ironman = new Toy
+    breakfast.addFoodItem(ironman)
+
     assertEquals(1.95, fry.price, 10e-5)
-    assertEquals(1.56, fry.discounted80, 10e-5)
 //    assertEquals(6.85, breakfast.price, 10e-5)
 
     assertEquals(1, fry.cost, 10e-5)
