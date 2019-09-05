15d7ed7e8f6c5615e928216d5ed099b7dba8cee9
mc <mc>
2019-09-05 15:40:20 -0700

    fully impl singleton

--- src/test/scala/org/vitalstar/TestComposite.scala
+++ src/test/scala/org/vitalstar/TestComposite.scala
@@ -4,70 +4,141 @@ import org.scalatest.{FunSuite, BeforeAndAfterAll}
 import org.scalatest.junit.JUnitRunner
 import org.junit.Assert._
 import org.junit.runner.RunWith
-import collection.mutable.ListBuffer
 
+import collection.mutable.ListBuffer
 // We define a concept, which is behavior
-trait FoodItemish {
+trait Item {
   def name: String = ""
   def sold: Boolean = false
   def consumed: Boolean = sold
   def calories: Double = 0.0
-  def addFoodItem(item: FoodItemish) = {}
-  def getFoodItems: List[FoodItemish] = List()
+  def addItem(item: Item) = {}
+  def getItems: List[Item] = List()
   def price: Double
   def isSingle: Boolean = true
 }
 
 // This is the implementation
-abstract class FoodItemImpl(_name: String ="", val _price: Double = 0.0)
-    extends FoodItemish {
+class ItemBase(_name: String ="", val _price: Double = 0.0)
+    extends Item {
 
   override val name: String = _name
-  val bag: ListBuffer[FoodItemish] = new ListBuffer[FoodItemish]()
+  val bag: ListBuffer[Item] = new ListBuffer[Item]()
 
-  override def addFoodItem(item: FoodItemish): Unit = bag.append(item)
-  override def getFoodItems: List[FoodItemish] = bag.toList
+  override def addItem(item: Item): Unit = bag.append(item)
+  override def getItems: List[Item] = bag.toList
   override def price: Double = {
     if (isSingle)
         _price
     else
-      bag.foldLeft(0.0){(price: Double, i: FoodItemish) => i.price + price}
+      bag.foldLeft(0.0){(price: Double, i: Item) => i.price + price}
   }
   override def toString: String = name
 }
 
-class Toy(name: String = "", _price: Double = 0.0)
-  extends FoodItemImpl(name, _price) {
+class Food(name:String, _price: Double) extends ItemBase(name, _price) {
+  override def addItem(item: Item): Unit = {}
+  override def getItems: List[Item] = List(this)
 }
 
-class Food(name:String, _price: Double) extends FoodItemImpl(name, _price) {
-  override def addFoodItem(item: FoodItemish): Unit = {}
-  override def getFoodItems: List[FoodItemish] = List(this)
-}
-
-class Meal(name: String)  extends FoodItemImpl(name) {
+class Meal(name: String)  extends ItemBase(name) {
   override def isSingle = false
-  override def toString = name + " " + getFoodItems.toString
+  override def toString = name + " " + getItems.toString
 }
 
+class Toy(name: String = "", _price: Double = 0.0)
+  extends ItemBase(name, _price) {
+}
+
+class Store(address: String) {
+  val shelf: ListBuffer[Item] = new ListBuffer[Item]()
+  def replenish(items: List[Item]): Unit = shelf.appendAll(items)
+  def numberOfItems = shelf.length
+  def order(name: String): Item = {
+    val items = shelf.filter{e => e.name == name}
+    if (!items.isEmpty) {
+      val item = items.head
+      shelf -= item
+      item
+    } else null
+  }
+
+  override def toString = shelf.length + " items in" + address
+}
+
+class World {
+  private var aug: Store = null
+
+  def getStore: Store = {
+    if (aug == null) {
+      aug = new Store("1500 Augustine drive")
+    }
+    aug
+  }
+
+  def reset = {
+    aug = null
+  }
+}
 
 @RunWith(classOf[JUnitRunner])
 class TestComposite extends FunSuite with BeforeAndAfterAll {
+  val earth = new World()
+
+  // Create some items
+  val fry = new Food("fry",1.95)
+  val burger = new Food("double double",3.40)
+  val drink = new Food("soda", 1.5)
+  val breakfast = new Meal("snack")
+  breakfast.addItem(fry)
+  breakfast.addItem(burger)
+  breakfast.addItem(drink)
 
   test("Encapsulation") {
-    val fry = new Food("fry",1.95)
-    val burger = new Food("double double",3.40)
-    val drink = new Food("soda", 1.5)
-    val breakfast = new Meal("snack")
-    breakfast.addFoodItem(fry)
-    breakfast.addFoodItem(burger)
-    breakfast.addFoodItem(drink)
-
     val ironman = new Toy
-    breakfast.addFoodItem(ironman)
+    breakfast.addItem(ironman)
 
     assertEquals(1.95, fry.price, 10e-5)
     assertEquals(6.85, breakfast.price, 10e-5)
   }
 
+  test("create store") {
+    // Create a store
+    val store = new Store("1500 Augustine drive")
+    store.replenish(List.fill(10)(fry))
+    store.replenish(List.fill(10)(burger))
+    store.replenish(List.fill(10)(drink))
+    store.replenish(List.fill(10)(breakfast))
+
+    val food = store.order("fry")
+    assertEquals(39, store.numberOfItems)
+
+    val meal = store.order("snack")
+    assertEquals(38, store.numberOfItems)
+  }
+
+  test("singleton") {
+    var aug = earth.getStore
+    assertEquals(0, aug.numberOfItems)
+
+    aug = earth.getStore
+    aug.replenish(List.fill(10)(fry))
+    assertEquals(10, aug.numberOfItems)
+
+    aug = earth.getStore
+    aug.replenish(List.fill(10)(burger))
+    assertEquals(20, aug.numberOfItems)
+
+    aug = earth.getStore
+    aug.replenish(List.fill(10)(drink))
+    assertEquals(30, aug.numberOfItems)
+
+    aug = earth.getStore
+    aug.replenish(List.fill(10)(breakfast))
+    assertEquals(40, aug.numberOfItems)
+
+    earth.getStore.order("fry")
+    assertEquals(39, aug.numberOfItems)
+
+  }
 }
